4daae8a1e3054fccbc23eadd926d8a6939852b0f
dmt.malin <dmt.malin@gmail.com>
2017-05-03 16:04:02 +0300

    Update graphql request

--- src/ledbillboard/playlist/schema.py
+++ src/ledbillboard/playlist/schema.py
@@ -9,11 +9,14 @@ from ledbillboard.references.models import Schedule
 
 
 class ScheduleType(DjangoObjectType):
+    id = graphene.Int()
+
     class Meta:
         model = Schedule
 
 
 class MediaType(DjangoObjectType):
+    id = graphene.Int()
     url = graphene.String()
 
     def resolve_url(self: Media, args, context, info):
@@ -25,9 +28,17 @@ class MediaType(DjangoObjectType):
 
 class PlaylistType(DjangoObjectType):
     media = graphene.List(MediaType)
+    base_id = graphene.Int()
+    company = graphene.String()
 
     def resolve_media(self: Playlist, args, context, info):
         return Media.objects.filter(playlist=self, is_approve=True)
+
+    def resolve_base_id(self: Playlist, args, context, info):
+        return self.id
+
+    def resolve_company(self: Playlist, args, context, info):
+        return self.board.company.name
 
     class Meta:
         model = Playlist
